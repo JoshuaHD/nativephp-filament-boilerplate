9d16f2fe597aa90622e45d7bad056b9a06ba2ee5
JoshuaHD <22371601+JoshuaHD@users.noreply.github.com>
2026-06-05 18:27:53 -0400

    feat: wire Filament admin panel to the root path

--- app/Http/Middleware/AutoLoginLocalUser.php
+++ app/Http/Middleware/AutoLoginLocalUser.php
@@ -1,8 +1,32 @@
 <?php
 
-namespace App\Middleware;
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class AutoLoginLocalUser
 {
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (! app()->isLocal()) {
+            return $next($request);
+        }
 
+        $user = User::firstOrCreate(
+            ['email' => 'owner@local'],
+            [
+                'name' => 'Owner',
+                'password' => Hash::make('0000'),
+            ]
+        );
+
+        Auth::login($user);
+
+        return $next($request);
+    }
 }
